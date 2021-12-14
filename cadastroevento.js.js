93472af89b25a10b9b696225a7f8dc765a648d21
leonardorocha <lk3890@hotmail.com>
2021-12-14 20:57:37 -0300

    atividade online 01

---
+++ cadastroevento.js.js
@@ -0,0 +1,46 @@
+    //condição para data do evento
+
+    let datahoje = 
+    let dataevento = 
+
+    if (datahoje <= dataevento){
+        console.log("Evento não disponivel")
+    } else {
+        console.log("Evento disponivel")
+    }
+
+    //condição para idade 
+
+    let idadeatual = 
+    let idadepermitida = 18
+
+    if (idadeatual >= 18){
+        console.log("Cadastro permitido")
+    } else {
+        console.log("Cadastro permitido somente para maiores de idade")
+    }
+
+    //condição para quantidade de participantes
+
+    let totalparticipantes = 100
+    let quantidadeatual = 
+
+    if (quantidadeatual > 100){
+        console.log("Não há mais vagas para o evento")
+    } else {
+        console.log("Há vagas ainda para o evento")
+    }
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
